a41ac602500397d4f73fddcf4bacd0d7f74a974d
Mikhail Sazanov <m@sazanof.ru>
2023-08-19 09:53:27 +0300

    update getMailboxByPath method

--- src/Response/SingleMailboxResponse.php
+++ src/Response/SingleMailboxResponse.php
@@ -11,8 +11,8 @@ class SingleMailboxResponse
 	public function __construct(string $path, Response $response)
 	{
 		foreach ($response->lines() as $line) {
-			if (preg_match('/\* LIST \((.*?)\) (.*?) "(.*?)"/', $line, $matches)) {
-				if ($matches[3] === $path) {
+			if (preg_match('/\* LIST \((.*?)\) (.*?) (.*?)$/', $line, $matches)) {
+				if (trim($matches[3]) === $path) {
 					$this->mailbox = new Mailbox($line);
 				}
 			}
